c2c712d4153decad7d6ffb6d4ecf995d0475f48c
Kawika Kekahuna <kawikadkekahuna@github.com>
2015-05-28 19:28:11 -1000

    calculates highest prime factor

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,27 @@
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
 
-  return primeNumber;
+   var primeNumber = 0;
+   var start = 2;
+   var highestFactor = 0;
+
+
+
+	while(n != 1){
+
+	if(n % start === 0){
+	highestFactor = start;
+	n = n / start;
+	
+	}else{
+		start++;
+	}
+
+
+
+
+
+
+}
+	return highestFactor;
+
 };
